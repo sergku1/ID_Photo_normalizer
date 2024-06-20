f74dc0361cac173a9b7b088f0f0fbaae3fc87c35
sergku1 <167416357+sergku1@users.noreply.github.com>
2024-06-20 19:22:07 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,29 @@
+"""Это конфигурационны файл для сохранения путей к файлам и других настроек версия для размещения в dataFiles"""
+# Путь к папке с входящими изображениями
+imageDir = '/home/sergku/PrgM/V2/Img'
+
+# Допустимые форматы файлов изображений
+imageExt = ['.jpeg', '.jpg', '.bmp', '.png', '.gif']
+
+# Путь к домашней папке
+homePath = '/home/sergku'
+
+# Путь к папке с записями клиентов
+infoPath = '/home/sergku/PrgM/V2/Info'
+
+# Путь к папке для отработанных файлов
+trashDir = '/home/sergku/PrgM/V2/Trash'
+
+# Путь к папке основной информации
+dataFiles = '/home/sergku/PrgM/V2'
+
+# Путь к папке с временными файлами
+Temp = '/home/sergku/PrgM/V2/tmpDir'
+
+# Путь к папке со служебными файлами программы
+Calculon = '/home/sergku/PrgM/V2/'
+
+# Стандартные размеры для вывода изображения Shir = width, Vys=height
+Shir = 720
+Vys = 900
+Vys2 = 500
